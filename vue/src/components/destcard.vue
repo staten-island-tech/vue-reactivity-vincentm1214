8740990940af99edd147de6b3bfbabcc521a7086
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-02-05 10:13:44 -0500

    oops

---
+++ vue/src/components/destcard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>{{ Destination.name }}</h2>
+        <img :src="Destination.img" alt="">
+        <h3>{{ clicked }}</h3>
+        <button @click="increment">click me</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props= defineProps({
+    Destination: Object,
+   });
+
+   //clicker logic
+   let clicked= ref(0);
+   function increment(){
+    clicked.value++;
+   }
+</script>
+    
+
+<style scoped>
+
+img{
+    width: 250px;
+    height: 500 px;
+}
+</style>
