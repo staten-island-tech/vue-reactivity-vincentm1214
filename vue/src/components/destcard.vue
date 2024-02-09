79b9bc70564b28cd6f123e94e1c1eebeea916cdb
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-02-09 10:12:54 -0500

    :moyai:

--- vue/src/components/destcard.vue
+++ vue/src/components/destcard.vue
@@ -1,30 +1,28 @@
 <template>
     <div>
-        <h2>{{ Destination.name }}</h2>
-        <img :src="Destination.img" alt="">
-        <h3>{{ clicked }}</h3>
-        <button @click="increment">click me</button>
+      <h1>{{ Destination.name }}</h1>
+      <img :src="Destination.img" alt="" />
+      <h2>{{ clicked }}</h2>
+      <button @click="increment">Click ME</button>
     </div>
-</template>
-
-<script setup>
-import {ref} from "vue";
-const props= defineProps({
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  const props = defineProps({
     Destination: Object,
-   });
-
-   //clicker logic
-   let clicked= ref(0);
-   function increment(){
+  });
+  //clicker logic
+  const clicked = ref(0);
+  function increment() {
     clicked.value++;
-   }
-</script>
-    
-
-<style scoped>
-
-img{
+  }
+  </script>
+  
+  <style scoped>
+  img {
     width: 250px;
-    height: 500 px;
-}
-</style>
+    height: 300px;
+    object-fit: cover;
+  }
+  </style>
